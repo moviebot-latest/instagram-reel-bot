7d55d49badd977aa3822b9448bdc405c0f96ca64
moviebot-latest <amansahu0032@gmail.com>
2026-03-09 21:47:40 +0530

    Add configuration for bot and media files

---
+++ config.py
@@ -0,0 +1,11 @@
+import os
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+INSTAGRAM_ACCESS_TOKEN = os.getenv("INSTAGRAM_ACCESS_TOKEN")
+
+INSTAGRAM_ACCOUNT_ID = os.getenv("INSTAGRAM_ACCOUNT_ID")
+
+CLIP_FOLDER = "clips/"
+MUSIC_FILE = "music/bg_music.mp3"
+OUTPUT_FILE = "output/final_reel.mp4"
