e2ad5502f020c9395ae4b2c5b4396dd16121a97c
moviebot-latest <amansahu0032@gmail.com>
2026-03-10 06:22:19 +0530

    Refactor threading for scheduler and Telegram listener

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask
 import threading
 import scheduler
-import telegram_fetch
+from telegram_fetch import start_telegram_listener
 
 app = Flask(__name__)
 
@@ -9,18 +9,18 @@ app = Flask(__name__)
 def home():
     return "Auto Reel Bot Running"
 
+
 def run_scheduler():
-    print("Scheduler starting...")
     scheduler.start_scheduler()
 
-def run_telegram():
-    print("Telegram bot starting...")
-    telegram_fetch.start_telegram_listener()
-
-# background threads
-threading.Thread(target=run_scheduler).start()
-threading.Thread(target=run_telegram).start()
 
 if __name__ == "__main__":
-    print("Server starting...")
+
+    # scheduler background
+    threading.Thread(target=run_scheduler).start()
+
+    # telegram bot MAIN THREAD
+    start_telegram_listener()
+
+    # web server
     app.run(host="0.0.0.0", port=10000)
